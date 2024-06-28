c004800e8c16e7f4ece89bdd9e1fc57ac2ceb521
Abylkaiyr <abilkhayir98@gmail.com>
2024-06-28 17:06:29 +0500

    Refactoring the fetch games with react query

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,6 +1,8 @@
+import { useQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
-import useData from "./useData";
-import { Genre } from "./useGenres";
+import { FetchResponse } from "../services/api-client";
+import apiClient from "../services/api-client";
+
 
 
 export interface Platform {
@@ -10,22 +12,30 @@ export interface Platform {
 }
 
 export interface Game {
-     id: number;
-     name: string;
-     background_image: string;
-     parent_platforms: { platform: Platform }[]; 
-     metacritic: number;
-     rating_top: number;
+  id: number;
+  name: string;
+  background_image: string;
+  parent_platforms: { platform: Platform }[];
+  metacritic: number;
+  rating_top: number;
 }
-   
-  
 
-const useGames = (gameQuery: GameQuery) => useData<Game>("/games", { params:
-   { genres: gameQuery.genre?.id,
-     platforms: gameQuery.platform?.id,
-      ordering: gameQuery?.sortBy,
-      search: gameQuery.searchText 
-    },    
-    }, [gameQuery]);
+
+
+const useGames = (gameQuery: GameQuery) => useQuery<FetchResponse<Game>, Error>(
+  {
+    queryKey: ["games", gameQuery],
+    queryFn: () =>
+      apiClient.get<FetchResponse<Game>>("/games", {
+        params: {
+          genres: gameQuery.genre?.id,
+          parent_platforms: gameQuery.platform?.id,
+          ordering: gameQuery.sortBy,
+          search: gameQuery.searchText,
+        },
+      })
+        .then((response) => response.data),
+  }
+)
 
 export default useGames;
--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -2,7 +2,7 @@
 
 import { useQuery } from "@tanstack/react-query";
 import apiClient from "../services/api-client";
-import { FetchResponse } from "./useData";
+import { FetchResponse } from "../services/api-client";
 import genres from "../assets/data/genres";
 export interface Genre {
      id: number;
